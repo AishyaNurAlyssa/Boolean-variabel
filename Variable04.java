cd2cb552ff538b6791b8cfc77a5244bc75123bed
AishyaNurAlyssa <aishyanuralyssa025@gmail.com>
2024-09-21 20:44:20 +0800

    Add files via upload

---
+++ Variable04.java
@@ -0,0 +1,21 @@
+public class Variable04 {
+    public static void main(String[] args) {
+        //deklarasi
+        int bil1, bil2;
+        boolean status;
+
+        //input 
+        bil1 = 4;
+        bil2 = 7;
+
+        //proses
+        //status = bil1 > bil2;
+        //status = bil1 <= bil2;
+       status = bil1 >= bil2;
+         //status = bil1 == bil2;
+        //status = bil1 != bil2;
+
+        //output
+        System.out.println(status);
+    }
+}
